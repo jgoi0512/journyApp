37f38feb8bf3dfb026d6e19ef75b6c9d4456622f
jgoi0512 <jgoi051203@gmail.com>
2024-04-23 10:20:11 +1000

    Initial Commit

---
+++ Journy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journy
+//
+//  Created by Justin Goi on 23/4/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
